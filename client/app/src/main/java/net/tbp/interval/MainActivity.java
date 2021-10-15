d9861805d5f9db4f1548101a73a98243c2096cbc
Luksawee Phansri <luksawee.phansri@gmail.com>
2021-10-15 16:56:58 -0700

    add top right action bar button that will use to link to backup

--- client/app/src/main/java/net/tbp/interval/MainActivity.java
+++ client/app/src/main/java/net/tbp/interval/MainActivity.java
@@ -1,47 +1,72 @@
 package net.tbp.interval;
 
 import android.os.Bundle;
-
-import com.example.interval.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.Menu;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.interval.R;
 import com.example.interval.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // Make sure this is before calling super.onCreate
         super.onCreate(savedInstanceState);
-        // set the theme for the mainsreen
-        setTheme(R.style.Theme_Interval);
+        setTheme(R.style.Theme_Interval_NoActionBar);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_calendar, R.id.navigation_reminder, R.id.navigation_tracker)
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_calendar, R.id.navigation_reminder, R.id.navigation_tracker,
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+        )
+                .setOpenableLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
-
+        BottomNavigationView bottom_nav_view = findViewById(R.id.bottom_nav_view);
+        NavigationUI.setupWithNavController(bottom_nav_view, navController);
     }
 
-    //test1
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-    //test2
-
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
 }
