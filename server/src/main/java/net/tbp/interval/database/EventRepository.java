7672d1af42b8ea764a03baa5ad1178e503290fab
Jade Webb <66337093+jadewebb@users.noreply.github.com>
2021-10-18 23:30:53 -0700

    Add SQL Queries for SELECT, INSERT, UPDATE, DELETE

--- server/src/main/java/net/tbp/interval/database/EventRepository.java
+++ server/src/main/java/net/tbp/interval/database/EventRepository.java
@@ -9,22 +9,24 @@ import net.tbp.interval.backup.Event;
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
 	
-	@Query("TODO: WRITE THE CORRECT MYSQL QUERY")
+	@Query(SELECT * FROM currentevent WHERE eventID IN (SELECT eventID FROM userhascurrentevent WHERE userID = uid))
 	// WARNING: ORDER OF PARAMETERS MAY NOT BE CORRECT
 	// THEY MUST MATCH THE ORDER OF PARAM IN THE QUERY CALL
 	List<Event> findAllCurrentEvents(Integer uid);
 	
-	@Query("TODO: WRITE THE CORRECT MYSQL QUERY")
+	@Query(INSERT INTO currentevent(eventID, name, description, location, category, alarm, start, end) VALUES(newEvent.getID(), newEvent.getName(), newEvent.getDescription(), newEvent.getLocation(), newEvent.getCategory(), newEvent.getAlarm(), newEvent.getStartTime(), newEvent.getEndTime()))
+	@Query(INSERT INTO userhascurrentevent VALUES (uid, newEvent.getID))
 	// WARNING: ORDER OF PARAMETERS MAY NOT BE CORRECT
 	// THEY MUST MATCH THE ORDER OF PARAM IN THE QUERY CALL
 	void addNewCurrentEvent(Integer uid, Event newEvent);
 	
-	@Query("TODO: WRITE THE CORRECT MYSQL QUERY")
+	@Query(UPDATE currentevent SET name = newEvent.getName(), description = newEvent.getDescription(), location = newEvent.getLocation(), category = newEvent.getCategory(), alarm = newEvent.getAlarm(), start = newEvent.getStartTime(), end = newEvent.getEndTime() WHERE eventID = eventid)
 	// WARNING: ORDER OF PARAMETERS MAY NOT BE CORRECT
 	// THEY MUST MATCH THE ORDER OF PARAM IN THE QUERY CALL
 	void updateCurrentEvent(Integer uid, Integer eventid, Event newEvent);
 	
-	@Query("TODO: WRITE THE CORRECT MYSQL QUERY")
+	@Query(DELETE FROM currentevent WHERE eventID = eventid)
+	@Query(DELETE FROM userhascurrenteent WHERE userID = uid AND eventID = eventid)
 	// WARNING: ORDER OF PARAMETERS MAY NOT BE CORRECT
 	// THEY MUST MATCH THE ORDER OF PARAM IN THE QUERY CALL
 	void deleteCurrentEvent(Integer uid, Integer eventid);
