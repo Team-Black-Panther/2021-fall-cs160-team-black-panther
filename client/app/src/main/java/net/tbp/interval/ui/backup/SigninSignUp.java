a46bd1f16b290a8cd39d83f6b668a4aaef909863
Luksawee Phansri <luksawee.phansri@gmail.com>
2021-11-27 12:29:15 -0800

    logic for signin and sign up class

--- client/app/src/main/java/net/tbp/interval/ui/backup/SigninSignUp.java
+++ client/app/src/main/java/net/tbp/interval/ui/backup/SigninSignUp.java
@@ -2,23 +2,40 @@ package net.tbp.interval.ui.backup;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
+
 import com.example.interval.R;
 
-public class SigninSignUp extends Activity {
+public class SigninSignUp extends Activity{
+    String TAG = "SigninSignUpFragment";
+    String TAGSQL = "UserSQL";
     private Button signinBtn;       // button for user to go sign in
     private Button signupBtn;       // button for user to go sign up
+    int userCount = 0;
     protected void onCreate(Bundle savedInstanceState) {
         // set theme for screen
         setTheme(R.style.blackBackground);
         // create the screen
         super.onCreate(savedInstanceState);
-        // load content from add_reminder fragment to render the screen
         setContentView(R.layout.sign_in_and_sign_up);
-
+        // load user database
+        Cursor uri = getContentResolver().query(UserProvider.CONTENT_URI, null, null, null, null);
+        userCount = uri.getCount();
+        Log.d("userCount", String.valueOf(userCount));
+        if(userCount >= 1){
+            finish();
+        }
         signinBtn = (Button) findViewById(R.id.sign_inBtn);
         signinBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,4 +58,6 @@ public class SigninSignUp extends Activity {
             }
         });
     }
+
+
 }
